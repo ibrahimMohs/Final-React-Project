ef9d54144eb89b66ff8cabad5c61669c73ba9dd7
Nailyaaaa <nailaabishova23@gmail.com>
2023-11-26 21:58:56 +0400

    login button side

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState} from 'react';
 import './App.css';
+import { User } from './models/user';
 import { HomePage } from './pages/HomePage/HomePage';
 // import { Route, Routes } from 'react-router-dom';
 import { Header } from 'antd/es/layout/layout';
@@ -10,21 +11,47 @@ import LoginPage from '../src/pages/LoginPage/LoginPage';
 
 
 function App() {
+  const [userInfo, setUserInfo] = useState<User | undefined>(undefined);
+
+  function handleLogIn(user: User) {
+    setUserInfo(user);
+  }
+function App() {
   return (
-    <>
+    <><Header userInfo={userInfo} />
+      {userInfo ?(
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
+         <Route
+            path="login"
+            element={<LoginPage logInHandler={handleLogIn} />}
+          />
+          </Routes>
+      ) : (
+        <Routes>
+       <Route
+            index
+            path="/"
+            element={<LoginPage logInHandler={handleLogIn} />}
+          />
+           <Route
+            path="login"
+            element={<LoginPage logInHandler={handleLogIn} />}
+          />
+          </Routes>
 
         {/* Using path="*"" means "match anything", so this route
               acts like a catch-all for URLs that we don't have explicit
               routes for. */}
         <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      )}
+      
       <LoginPage />
       <HomePage />
     </>
   );
+  
 };
-
+};
 export default App;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,6 +3,20 @@ import { GoogleLogin } from "react-google-login";
 import { useNavigate, Link } from "react-router-dom";
 import "./LoginPage.scss";
 import background from '../../assets/images/loginphoto.jpg';
+import { User } from '../../models/user';
+
+type LoginPageProps = {
+  logInHandler: (user: User) => void;
+};
+const handleLoginButtonClick = () => {
+  // later user info will be received from the backend
+  const user: User = {
+    login: "Admin",
+    name: "userName",
+  };
+  props.logInHandler(user);
+};
+ 
 
 const GoogleSignIn = () => {
   const [rememberMe, setRememberMe] = useState(false);
@@ -24,64 +38,69 @@ const GoogleSignIn = () => {
   const responseGoogle = (response: any) => {
     console.log(response);
     navigate("/dashboard");
+    
   };
+
   return (
     <>
-    <div className="main">
-
-    
-    <img src={background} alt="" className="background"/>
-    <div className="allofdiv">
-      <div className="login-page">
-      <h1>WELCOME MOVIES WEBSITE </h1>
-        <div className="container">
-          <form>
-            <label className="email">
-              Email
-              <input className="email" type="text" />
-            </label>
-            <label className="email">
-              Password
-              <input className="email" type="password" />
-            </label>
-            <div>
-              <button>Sign In</button>
+      <div className="main">
+        <img src={background} alt="" className="background" />
+        <div className="allofdiv">
+          <div className="login-page">
+            <h1>WELCOME MOVIES WEBSITE </h1>
+            <div className="container">
+              <form>
+                <label className="input">
+                  Email
+                  <div>
+                    <input className="textplace" type="text" />{" "}
+                  </div>
+                </label>
+                <label className="email">
+                  Password
+                  <div>
+                    {" "}
+                    <input className="textplace" type="password" />{" "}
+                  </div>
+                </label>
+                <div>
+                  <button type="button" onClick={handleLoginButtonClick}>Sign In</button>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
-      <GoogleLogin
-        clientId="YOUR_CLIENT_ID.apps.googleusercontent.com"
-        buttonText="Sign in with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-        className="google-login-button"
-        />
-      <div className="break">
-        <hr className="hr1" />
-        <p>OR</p>
-        <hr className="hr2" />
-      </div>
-      <div className="remember-forgotpassw">
-        <div>
-          <input
-            type="checkbox"
-            id="rememberMe"
-            checked={rememberMe}
-            onChange={(e) => setRememberMe(e.target.checked)}
+            <GoogleLogin
+              clientId="YOUR_CLIENT_ID.apps.googleusercontent.com"
+              buttonText="Sign in with Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              className="google-login-button"
             />
-          <label htmlFor="rememberMe">Remember Me</label>
+            <div className="break">
+              <hr className="hr1" />
+              <p>OR</p>
+              <hr className="hr2" />
+            </div>
+            <div className="remember-forgotpassw">
+              <div>
+                <input
+                  type="checkbox"
+                  id="rememberMe"
+                  checked={rememberMe}
+                  onChange={(e) => setRememberMe(e.target.checked)}
+                />
+                <label htmlFor="rememberMe">Remember Me</label>
+              </div>
+              <a href="/forgot-password">Forgot Password?</a>
+            </div>
+            <div className="login-footer">
+              <p>
+                Don't have an account? <Link to="/RegisterPage">Register</Link>
+              </p>
+            </div>
+          </div>
         </div>
-        <a href="/forgot-password">Forgot Password?</a>
       </div>
-      <div className="login-footer">
-        <p>
-          Don't have an account? <Link to="/RegisterPage">Register</Link>
-        </p>
-      </div>
-      </div>
-    </div>
-    </div>
     </>
   );
 };
